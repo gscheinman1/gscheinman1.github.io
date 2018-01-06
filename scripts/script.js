56ef64ed16af65017eb636ece55c6b858eda950e
adrayv <draym001@g.ucla.edu>
2018-01-06 00:34:17 -0800

    fully responsive reasons bg

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,14 @@
+//var md-width = 992;
 $(document).ready(function(){
 	$("#mobile-menu-btn").click(function() {
 		$("#mobile-menu-dropdown").toggle();
 	});
-	$(window).on('resize', function(){
+	$(window).on('resize load', function(){
 		$("#mobile-menu-dropdown").hide();
+		var height = $("#reason-area").height();
+		var newVal = (height + 150).toString() + "px";
+		var transformVal = "translate(0, " + newVal + ")";
+		$("#reason-bg-middle").css("height", newVal);
+		$("#reason-bg-bottom").css("transform", transformVal);
 	});
 });
